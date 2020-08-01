9abf8559e863e52d8d3a8924a17c054db220158c
G.Charkiewicz <charkiewicz.grzegorz@gmail.com>
2020-08-01 15:08:12 +0200

    Numbers.

---
+++ Basics Variables and Flow Control/numbers.js
@@ -0,0 +1,25 @@
+let num = 5.5 /2;
+
+let x = 11;
+let number = x + 1 * 2;
+
+console.log(num);
+console.log(number);
+
+let numberValue = (x + 1) * 2;
+console.log(numberValue);
+
+
+let age = 26;
+let dogYears = (age + 1) / 7;
+
+console.log(dogYears);
+
+// studentScore
+// maxScore
+// percent 90
+
+let studentScore = 18;
+let maxScore = 20;
+let percent = (studentScore / maxScore ) * 100;
+console.log(percent);
