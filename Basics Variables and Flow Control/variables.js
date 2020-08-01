e123c2fe307ea1fc95121a109c3b3cbfcd5e11e3
G.Charkiewicz <charkiewicz.grzegorz@gmail.com>
2020-08-01 15:15:16 +0200

    More on Variables.

---
+++ Basics Variables and Flow Control/variables.js
@@ -0,0 +1,16 @@
+// 1.
+let petName = 'Hal';
+//let petName = 'Spot'; has already been declared
+petName = 'Sport';
+
+console.log(petName);
+
+// 2.
+// let 3 = 2; SyntaxError: Unexpected number
+let a = 2;
+let result = a + 4;
+console.log(result);
+
+// 3.
+
+//let let = 12 // SyntaxError: let is disallowed as a lexically bound name
