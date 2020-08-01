648d62a1f855e7b54c6a4ef98865d18464a1ae37
G.Charkiewicz <charkiewicz.grzegorz@gmail.com>
2020-08-01 15:30:13 +0200

    Booleans and Comparison Operators.

---
+++ Basics Variables and Flow Control/boolean.js
@@ -0,0 +1,19 @@
+let temp = 31;
+let isFreezing = temp === 31;
+
+console.log(isFreezing);
+
+isFreezing = temp === 32;
+console.log(isFreezing);
+
+isFreezing = temp < 32;
+console.log(isFreezing);
+
+isFreezing = temp >= 32;
+console.log(isFreezing);
+
+let age = 26;
+let isChild = age <= 7;
+let isSenior = age >= 65;
+console.log(isChild);
+console.log(isSenior);
