fcc4f775e6a9aebaf31ce7422c0231b5c7103a50
G.Charkiewicz <charkiewicz.grzegorz@gmail.com>
2020-08-13 21:01:26 +0200

    Multiple Arguments and Argument Defaults

---
+++ Functions/arguments.js
@@ -0,0 +1,23 @@
+let add = function (a, b, c){
+    return a + b +c;
+};
+
+
+let result = add(10,1,5);
+
+console.log(result);
+
+let getScoreText = function (name = 'Anonymose', score = 0){
+    return 'Name ' + name + ' Score: ' + score;
+};
+
+let scoreText = getScoreText(undefined, 99);
+console.log(scoreText);
+
+
+let getTip = function(total, tipPercent = .2){
+    return 'Tip: ' + total * tipPercent;
+}
+
+let tip = getTip(40,undefined);
+console.log(tip);
