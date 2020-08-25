0fe8de772ca5b27dbef453da0e2dcd4c9618f67a
G.Charkiewicz <charkiewicz.grzegorz@gmail.com>
2020-08-25 16:58:06 +0200

    Looping Over Arrays.

---
+++ arrays/notes.js
@@ -0,0 +1,12 @@
+const nots = [
+    'Note 0',
+    'Note 1',
+    'Note 2',
+    'Note 3',
+];
+
+nots.forEach(function (item, index) {
+    let msg = `Element listy ${item} o indexsie ${index}`;
+    console.log(msg);
+});
+
