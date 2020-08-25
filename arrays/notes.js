e57ccd472f9c62d6f8aba8b32d080c2b67a4d59e
G.Charkiewicz <charkiewicz.grzegorz@gmail.com>
2020-08-25 20:26:25 +0200

    Searching Arrays: Part I

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,12 +1,28 @@
-const nots = [
-    'Note 0',
-    'Note 1',
-    'Note 2',
-    'Note 3',
-];
+const notes = [{
+    title: 'my next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise. Eating a bit better.'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
 
-nots.forEach(function (item, index) {
-    let msg = `Element listy ${item} o indexsie ${index}`;
-    console.log(msg);
-});
 
+
+const findNoteIndex = function (notes, noteTitle) {
+    return notes.findIndex(function (note, index) {
+        return note.title.toLowerCase() === noteTitle.toLowerCase()
+    })
+}
+
+const findNote = function (notes, noteTitle) {
+    return notes.find(function (note, index) {
+        return note.title.toLowerCase() === noteTitle.toLowerCase()
+    })
+}
+
+
+
+console.log( findNoteIndex(notes, 'Office modification') );
