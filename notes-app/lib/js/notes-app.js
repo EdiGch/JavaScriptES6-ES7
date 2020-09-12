922514246401b10c1a846fba0ccfaf1d6d6d43b7
G.Charkiewicz <charkiewicz.grzegorz@gmail.com>
2020-09-12 09:06:28 +0200

    DOM Manipulation.

---
+++ notes-app/lib/js/notes-app.js
@@ -0,0 +1,12 @@
+
+// Query and remove
+const p = document.querySelector('p');
+ //p.remove();
+
+ // Query all and remove
+const ps = document.querySelectorAll('p');
+ps.forEach(function(index){
+    //console.log( index.textContent );
+    //index.remove();
+});
+
