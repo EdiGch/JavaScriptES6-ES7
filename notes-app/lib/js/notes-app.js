8c339793652a933b985e186d2ea58c838ca865cf
G.Charkiewicz <charkiewicz.grzegorz@gmail.com>
2020-09-14 07:51:18 +0200

    Rendering Our Filtered Data.

--- notes-app/lib/js/notes-app.js
+++ notes-app/lib/js/notes-app.js
@@ -9,8 +9,35 @@ const notes = [{
     body: 'Get a new seat'
 }];
 
+const filters ={
+    searchText: ''
+}
+
+const renderNots = function(notes, filters){
+    const filteredNotes = notes.filter(function (note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
+    });
+
+    // let removeListNotes = document.querySelectorAll('.list-group-item');
+    // removeListNotes.forEach(function (note, index) {
+    //     note.remove();
+    // });
+    document.querySelector('#list-notes').innerHTML = '';
+
+    filteredNotes.forEach(function(note){
+       let element = document.createElement('p');
+       element.setAttribute('class', 'list-group-item');
+       element.textContent = note.title;
+       document.querySelector('#list-notes').appendChild(element);
+    });
+
+};
+
+renderNots(notes, filters);
+
 document.querySelector('#seach-notes').addEventListener('input', function (e) {
-    console.log(e.target.value)
+    filters.searchText = e.target.value
+    renderNots(notes, filters);
 })
 
 document.querySelector("#create").addEventListener('click', function (e) {
