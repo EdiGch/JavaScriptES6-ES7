61bcfb10c645ca6f5cadb1fb16b2d139de7c98f4
G.Charkiewicz <charkiewicz.grzegorz@gmail.com>
2020-09-16 08:14:28 +0200

    Refactor.

--- toDo-app/lib/js/todo-app.js
+++ toDo-app/lib/js/todo-app.js
@@ -1,77 +1,8 @@
-let notes = [];
+let notes = getSavedTodos();
 
 const filters ={
     searchText: '',
     hideComplete: false
-};
-
-// Check for existing saved data
-const notesJSON =  localStorage.getItem('notes');
-if(notesJSON !== null){
-    notes = JSON.parse(notesJSON);
-}
-
-const structureHtmlToDoList = function(notes, filters){
-    let placeToCopy = document.getElementById('listToDo');
-
-    let filteredNotes = notes.filter(function (note) {
-        const searchTextMatch = note.title.toLowerCase().includes(filters.searchText.toLowerCase());
-        const hideCompleteMatch = !filters.hideComplete || !note.complete;
-        return searchTextMatch && hideCompleteMatch;
-
-        //return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
-    });
-
-    //filteredNotes = filteredNotes.filter(function (note) {
-        //return !filters.hideComplete || !note.complete;
-        // if(filters.hideComplete){
-        //     return !note.complete
-        // }else{
-        //     return true;
-        // }
-    //});
-
-    const incomplete = filteredNotes.filter(function (note) {
-        return !note.complete;
-    });
-    document.querySelector('#listToDo').innerHTML = '';
-
-    const summary = document.createElement('h2');
-    summary.textContent = `You have ${incomplete.length} todo left`;
-    placeToCopy.appendChild(summary);
-
-    filteredNotes.forEach(function (note) {
-
-        let aHref = document.createElement('a');
-        aHref.setAttribute('class', 'list-group-item list-group-item-action');
-
-        let divChild = document.createElement('div');
-        divChild.setAttribute('class', 'd-flex w-100 justify-content-between');
-
-        let title = document.createElement('h5');
-        title.setAttribute('class', 'mb-1 title-todo');
-
-        let small = document.createElement('small');
-        let description = document.createElement('p');
-        description.setAttribute('class', 'mb-1 description-todo');
-
-        placeToCopy.appendChild(aHref);
-        aHref.appendChild(divChild);
-
-
-        if(note.title.length > 0){
-            title.textContent = note.title;
-        }else{
-            title.textContent = 'Without title. System message.';
-        }
-
-        divChild.appendChild(title);
-        small.textContent = note.complete;
-        divChild.appendChild(small);
-        aHref.appendChild(description);
-
-    })
-
 };
 
 document.querySelector('#seach-notes').addEventListener('input', function (e) {
@@ -87,7 +18,8 @@ document.querySelector('#addNewElement').addEventListener('submit', function (e)
         title: e.target.elements.titleToDo.value,
         complete: e.target.elements.forFan.checked
     })
-    localStorage.setItem('notes', JSON.stringify(notes));
+    saveTodos(notes)
+
     structureHtmlToDoList(notes, filters);
     e.target.elements.titleToDo.value = '';
     e.target.elements.forFan.checked = '';
---
+++ toDo-app/lib/js/todo-functions.js
@@ -0,0 +1,86 @@
+// Fetch existing todos from localStorage
+const getSavedTodos = function () {
+    const todosJSON = localStorage.getItem('notes')
+
+    if (todosJSON !== null) {
+        return JSON.parse(todosJSON)
+    } else {
+        return []
+    }
+}
+
+// Save todos to localStorage
+const saveTodos = function (notes) {
+    localStorage.setItem('notes', JSON.stringify(notes))
+}
+
+// Render application todos based on filters
+const structureHtmlToDoList = function(notes, filters){
+    const placeToCopy = document.getElementById('listToDo');
+
+    let filteredNotes = notes.filter(function (note) {
+        const searchTextMatch = note.title.toLowerCase().includes(filters.searchText.toLowerCase());
+        const hideCompleteMatch = !filters.hideComplete || !note.complete;
+        return searchTextMatch && hideCompleteMatch;
+
+        //return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
+    });
+
+    //filteredNotes = filteredNotes.filter(function (note) {
+    //return !filters.hideComplete || !note.complete;
+    // if(filters.hideComplete){
+    //     return !note.complete
+    // }else{
+    //     return true;
+    // }
+    //});
+
+    const incomplete = filteredNotes.filter(function (note) {
+        return !note.complete;
+    });
+    document.querySelector('#listToDo').innerHTML = '';
+    placeToCopy.appendChild(generateSummaryDOM(incomplete));
+
+    filteredNotes.forEach(function (note) {
+        generateTodoDOM(note, placeToCopy)
+    })
+
+};
+
+// Get the DOM elements for an individual note
+const generateTodoDOM = function (note, placeToCopy) {
+    let aHref = document.createElement('a');
+    aHref.setAttribute('class', 'list-group-item list-group-item-action');
+
+    let divChild = document.createElement('div');
+    divChild.setAttribute('class', 'd-flex w-100 justify-content-between');
+
+    let title = document.createElement('h5');
+    title.setAttribute('class', 'mb-1 title-todo');
+
+    let small = document.createElement('small');
+    let description = document.createElement('p');
+    description.setAttribute('class', 'mb-1 description-todo');
+
+    placeToCopy.appendChild(aHref);
+    aHref.appendChild(divChild);
+
+
+    if(note.title.length > 0){
+        title.textContent = note.title;
+    }else{
+        title.textContent = 'Without title. System message.';
+    }
+
+    divChild.appendChild(title);
+    small.textContent = note.complete;
+    divChild.appendChild(small);
+    aHref.appendChild(description);
+}
+
+// Get the DOM elements for list summary
+const generateSummaryDOM = function (incompleteTodos) {
+    const summary = document.createElement('h2')
+    summary.textContent = `You have ${incompleteTodos.length} todos left`
+    return summary
+}
